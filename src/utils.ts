6fb76a8fea8c52c8f727d5f5a9e97cd8c073925f
Deep Toaster <deeptoaster@gmail.com>
2024-03-16 19:59:23 -0400

    move utils to dedicated folder

--- src/utils.ts
+++
@@ -1,5 +0,0 @@
-export { default as findPossibleDuplicates } from './functions/findPossibleDuplicates';
-export { default as loadDatabase } from './functions/loadDatabase';
-export { default as makeArtistName } from './functions/makeArtistName';
-export { default as remapDuplicates } from './functions/remapDuplicates';
-export { default as splitArtist } from './functions/splitArtist';
---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+export { default as findPossibleDuplicates } from './findPossibleDuplicates';
+export { default as loadDatabase } from './loadDatabase';
+export { default as makeArtistName } from './makeArtistName';
+export { default as remapDuplicates } from './remapDuplicates';
+export { default as splitArtist } from './splitArtist';
